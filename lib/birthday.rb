73bc9f9c3e9a9cb7137a9b521692fdc9fc124654
Matthew Easley <matteasley8791@gmail.com>
2021-01-22 16:06:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,8 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
+ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-  puts "Happy Birthday #{kids_name}! Your are now #{age} years old!"
-   end
- end 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
